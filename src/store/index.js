7b022569f7155c93681eaa9f1fbee2fa07033129
KaungMyatThway <scm.kaungmyatthway@gmail.com>
2022-06-01 10:57:55 +0630

    add vuex practice project

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,35 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    counter : 0,
+    colorCode : "blue"
   },
   getters: {
+    counterSquare (state) {
+      return state.counter * state.counter
+    }
   },
   mutations: {
+    increaseCount (state,increaseNumber) {
+      state.counter += increaseNumber
+    },
+     decreaseCount (state,decreaseNumber) {
+      state.counter-= decreaseNumber
+    }
   },
   actions: {
+    increaseCount ({commit}) {
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response=>{
+            commit('increaseCount',response.data)
+      })
+    },
+    decreaseCount ({commit}) {
+      axios('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new').then(response=>{
+            commit('decreaseCount',response.data)
+      })
+    },
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="counter" :style="{color:$store.state.colorCode}">
+      {{$store.state.counter}}
+    </div>
+    <div class="counter-square">
+      {{$store.state.counter}}
+      <sup>2</sup>
+      ={{$store.getters.counterSquare}}
+    </div>
+    <div class="button">
+      <button @click="$store.dispatch('decreaseCount')">-</button>
+      <button @click="$store.dispatch('increaseCount')">+</button>
+    </div>
+    <div class="colorInput">
+      <input type="text" placeholder="Add color code here">
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style scoped>
+  div {
+    margin-bottom: 10px;
+  }
+
+  .counter {
+    font-size: 80px;
+  }
+
+  .button button {
+    font-size: 40px;
+    width: 100px;
+    margin: 0px 10px;
+  }
+</style>
